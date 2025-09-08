3b8bf54d5a79bf658ebf89603fe3d11c150c35d3
nesj <alexanmiro@gmail.com>
2025-09-09 01:11:14 +0400

    removed requests limiter

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,6 @@
 import express from 'express';
 import cors from 'cors';
 import axios, { AxiosRequestConfig } from 'axios';
-import rateLimit from 'express-rate-limit';
 import * as dotenv from 'dotenv';
 import net from 'net';
 import dns from 'dns';
@@ -44,19 +43,12 @@ const corsOptions = {
   credentials: true,
 };
 
-const generalLimiter = rateLimit({
-  windowMs: 60 * 1000,
-  max: 120,
-});
-
 const app = express();
 
 app.use(helmet());
 
 app.use(express.json({ limit: '1mb' }));
 app.use(express.text({ type: 'text/*', limit: '2mb' }));
-
-app.use(generalLimiter);
 
 app.use((req, res, next) => {
   if (req.method === 'OPTIONS') {
